294270844de4d82c899f53ad006a8900728c8d69
Shahabaz Khan <shahbaz.9676@gmail.com>
2023-07-09 18:31:10 +0530

    Commit: Linting

--- src/components/button/styles.js
+++ src/components/button/styles.js
@@ -1,18 +1,18 @@
-import { StyleSheet } from 'react-native';
+import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
-    button: {
-        height:40,
-        width: 200,
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 14,
-        backgroundColor: 'orange',
-        borderRadius: 10
-    },
-    title: {
-        color: 'blue'
-    }
-})
+  button: {
+    height: 40,
+    width: 200,
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontSize: 14,
+    backgroundColor: 'orange',
+    borderRadius: 10,
+  },
+  title: {
+    color: 'blue',
+  },
+});
 
 export default styles;
