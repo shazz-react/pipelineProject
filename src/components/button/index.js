83ab866983431594538b5069800501b707e74507
Shahabaz Khan <shahbaz.9676@gmail.com>
2023-07-09 16:51:52 +0530

    Commit: Optimisation

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,12 +1,10 @@
 import { TouchableOpacity, Text } from "react-native";
 import styles from './styles';
 
-const Button = ({title, onPress}) => {
-    return (
-        <TouchableOpacity style={styles.button} onPress={onPress}>
-            <Text style={styles.title}>{title}</Text>
-        </TouchableOpacity>
-    )
-}
+const Button = ({title, onPress}) => (
+    <TouchableOpacity style={styles.button} onPress={onPress}>
+        <Text style={styles.title}>{title}</Text>
+    </TouchableOpacity>
+)
 
 export default Button;
